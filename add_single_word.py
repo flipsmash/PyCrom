5b778197e79bc3ddd4f362981599b82a3a72ec28
Brian Finnegan <brfinnegan@gmail.com>
2017-02-21 17:13:47 -0500

    Initial commit

--- add_single_word.py
+++ add_single_word.py
@@ -6,13 +6,20 @@ import json
 dictionary=PyDictionary()
 i=0
 while i != -1:
+    with open('definitions.txt') as json_data:
+        d = json.load(json_data)
     i = input("Enter the word you want added: ")
     dict = str(dictionary.meaning(i))
     if dict == "None":
         print("There was no definition for {} in WordNet.".format(i))
+        nd = input("Please enter the definition here:")
+        d[i]=nd
     else:
-        print("The WordNet definition is ", dictionary.meaning(i))
-
+        nd = dictionary.meaning(i)
+        print("The WordNet definition is ", nd)
+        d[i]=nd
+    with open('definitions.txt','w') as f:
+        json.dump(d,f)
     
         
     
